b94f91c8b8b42db3f95fdc9c56ae18ede97b557c
alini-ross <85803653+alini-ross@users.noreply.github.com>
2021-06-12 17:05:44 -0300

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$url = (isset($_GET['url'])) ? $_GET['url'] : 'home';
-$url = array_filter(explode('/', $url));
-
-
-require_once("controllers/site.php");
-$site = new SiteController();
-$site->index();
